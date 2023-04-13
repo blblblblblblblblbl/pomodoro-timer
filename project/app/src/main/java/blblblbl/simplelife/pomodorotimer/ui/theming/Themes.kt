bbfa0891dd404cdcf465d72d04ae82f92878d47c
Кирилл Толмачев <stitch00007@gmail.com>
2023-04-13 17:52:52 +0700

    material theme from color

--- project/app/src/main/java/blblblbl/simplelife/pomodorotimer/ui/theming/Themes.kt
+++ project/app/src/main/java/blblblbl/simplelife/pomodorotimer/ui/theming/Themes.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import blblblbl.simplelife.pomodorotimer.ui.theming.material_you_utils.scheme.Scheme
 
 private val LightColors = lightColorScheme(
     primary = md_theme_light_primary,
@@ -85,8 +86,43 @@ fun AppTheme(
         darkTheme -> DarkColors
         else -> LightColors
     }
+    val scheme =Scheme.light(Color.Blue.toArgb())
+    val colorScheme2 = ColorScheme(
+        primary= Color(scheme.primary),
+        onPrimary=Color(scheme.onPrimary),
+        primaryContainer=Color(scheme.primaryContainer),
+        onPrimaryContainer=Color(scheme.onPrimaryContainer),
+        inversePrimary=Color(scheme.inversePrimary),
+        secondary=Color(scheme.secondary),
+        onSecondary=Color(scheme.onSecondary),
+        secondaryContainer=Color(scheme.secondaryContainer),
+        onSecondaryContainer=Color(scheme.onSecondaryContainer),
+        tertiary=Color(scheme.tertiary),
+        onTertiary=Color(scheme.onTertiary),
+        tertiaryContainer=Color(scheme.tertiaryContainer),
+        onTertiaryContainer=Color(scheme.onTertiaryContainer),
+        background=Color(scheme.background),
+        onBackground=Color(scheme.onBackground),
+        surface=Color(scheme.surface),
+        onSurface=Color(scheme.onSurface),
+        surfaceVariant=Color(scheme.surfaceVariant),
+        onSurfaceVariant=Color(scheme.onSurfaceVariant),
+        surfaceTint=Color(scheme.surfaceVariant),
+        inverseSurface=Color(scheme.inverseSurface),
+        inverseOnSurface=Color(scheme.inverseOnSurface),
+        error=Color(scheme.error),
+        onError=Color(scheme.onError),
+        errorContainer=Color(scheme.errorContainer),
+        onErrorContainer=Color(scheme.onErrorContainer),
+        outline=Color(scheme.outline),
+        outlineVariant=Color(scheme.outlineVariant),
+        scrim=Color(scheme.scrim),
+    )
+    /*Scheme.light(Color.Blue.toArgb())
+    ColorScheme
+    CorePalette.of(Color.Blue.toArgb())*/
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colorScheme2,
         typography = UnsplashTypography,
         shapes = UnsplashShapes,
         content = content
