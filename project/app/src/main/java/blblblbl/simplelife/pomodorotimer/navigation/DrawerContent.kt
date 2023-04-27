2887ed3530a0d9b06c86bb7803d294dc7bcbed20
Кирилл Толмачев <stitch00007@gmail.com>
2023-04-27 21:28:49 +0700

    add rate app button on click

--- project/app/src/main/java/blblblbl/simplelife/pomodorotimer/navigation/DrawerContent.kt
+++ project/app/src/main/java/blblblbl/simplelife/pomodorotimer/navigation/DrawerContent.kt
@@ -1,17 +1,23 @@
 package blblblbl.simplelife.pomodorotimer.navigation
 
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import blblblbl.simplelife.pomodorotimer.ui.navigateSingleTopTo
 import kotlinx.coroutines.launch
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -19,6 +25,7 @@ fun DrawerContent(
     navController: NavHostController,
     drawerState: DrawerState
 ){
+    val context = LocalContext.current
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -33,10 +40,28 @@ fun DrawerContent(
         )
         Divider()
         Spacer(modifier = Modifier.weight(1F))
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = "wiki")
-        }
-        Button(onClick = { /*TODO*/ }) {
+
+        Button(onClick = {
+            try {
+                startActivity(
+                    context,
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("market://details?id=" + context.packageName)
+                    ),
+                    null
+                )
+            } catch (e: ActivityNotFoundException) {
+                startActivity(
+                    context,
+                    Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" + context.packageName)
+                    ),
+                    null
+                )
+            }
+        }) {
             Text(text = "rate app")
         }
     }
